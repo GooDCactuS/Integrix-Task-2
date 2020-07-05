bc9cd30752d771dfa8a0039538ceee5223a2750e
GooDCactuS <Marvele98@gmail.com>
2020-07-05 18:03:17 +0400

    docs: add methods' description

--- Wali-Express/Wali-Express.Core/ShopStorage.cs
+++ Wali-Express/Wali-Express.Core/ShopStorage.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 using Wali_Express.Core.Models;
 
 namespace Wali_Express.Core
@@ -10,14 +9,19 @@ namespace Wali_Express.Core
     public class ShopStorage
     {
         private readonly DatabaseContext _db;
-        public static IHost Host { get; set; }
+
 
         public ShopStorage(DatabaseContext context)
         {
-
             _db = context;
         }
 
+        /// <summary>
+        /// Adds a product to the database.
+        /// </summary>
+        /// <param name="name">A name of the product.</param>
+        /// <param name="amount">An amount of the product.</param>
+        /// <returns>Returns a product object if everything is okay, returns null otherwise.</returns>
         public async Task<Product> AddProduct(string name, int amount)
         {
             if (await _db.Products.FirstOrDefaultAsync(x => x.Name == name) != null)
@@ -28,6 +32,11 @@ namespace Wali_Express.Core
             return await AddProduct(product);
         }
 
+        /// <summary>
+        /// Adds a product to the database.
+        /// </summary>
+        /// <param name="product">A product object.</param>
+        /// <returns>Returns a product object if everything is okay, returns null otherwise</returns>
         public async Task<Product> AddProduct(Product product)
         {
             if (product == null) return null;
@@ -37,13 +46,23 @@ namespace Wali_Express.Core
             return product;
         }
 
+        /// <summary>
+        /// Returns a product from the database.
+        /// </summary>
+        /// <param name="name">A name of the product.</param>
+        /// <returns>Returns a product if it exists.</returns>
         public async Task<Product> GetProduct(string name)
         {
             var product = await _db.Products.FirstOrDefaultAsync(x => x.Name == name);
             return product;
         }
 
-
+        /// <summary>
+        /// Updates product in the database.
+        /// </summary>
+        /// <param name="name">A name of the product.</param>
+        /// <param name="amount">An amount of the product.</param>
+        /// <returns>Returns a product if everything is okay, returns null otherwise.</returns>
         public async Task<Product> UpdateProduct(string name, int amount)
         {
             var product = await _db.Products.FirstOrDefaultAsync(x => x.Name == name);
@@ -55,6 +74,12 @@ namespace Wali_Express.Core
             return await UpdateProduct(product, amount);
         }
 
+        /// <summary>
+        /// Updates a product in the database.
+        /// </summary>
+        /// <param name="product">The Product object.</param>
+        /// <param name="amount">An amount of the product.</param>
+        /// <returns>Returns a product object if everything is okay, returns null otherwise.</returns>
         public async Task<Product> UpdateProduct(Product product, int amount)
         {
             if (product == null) return null;
@@ -64,6 +89,13 @@ namespace Wali_Express.Core
             return product;
         }
 
+        /// <summary>
+        /// Adds a new reservation to the database.
+        /// </summary>
+        /// <param name="productName">A name of the product.</param>
+        /// <param name="amount">An amount of the product.</param>
+        /// <param name="client">A client's fullname.</param>
+        /// <returns>Returns a Reservation object if everything is okay, returns null otherwise.</returns>
         public async Task<Reservation> Reserve(string productName, int amount, string client)
         {
             var product = await _db.Products.FirstAsync(x => x.Name == productName);
@@ -79,6 +111,11 @@ namespace Wali_Express.Core
 
         }
 
+        /// <summary>
+        /// Adds a new reservation to the database.
+        /// </summary>
+        /// <param name="reservation">A reservation object.</param>
+        /// <returns>Returns a reservation object if everything is okay, returns null otherwise.</returns>
         public async Task<Reservation> Reserve(Reservation reservation)
         {
             if (reservation == null) return null;
@@ -88,6 +125,10 @@ namespace Wali_Express.Core
             return reservation;
         }
 
+        /// <summary>
+        /// Cancels reservation in the database.
+        /// </summary>
+        /// <param name="id">Id of the reservation</param>
         public async void CancelReserve(int id)
         {
             var reservation = await _db.Reservations.FirstAsync(x => x.Id == id);
@@ -97,6 +138,10 @@ namespace Wali_Express.Core
             }
         }
 
+        /// <summary>
+        /// Cancels reservation in the database.
+        /// </summary>
+        /// <param name="reservation">A reservation object.</param>
         public async void CancelReserve(Reservation reservation)
         {
             if (reservation == null) return;
@@ -110,6 +155,11 @@ namespace Wali_Express.Core
             await _db.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Deletes all reservation with specific product name.
+        /// </summary>
+        /// <param name="productName">A name of the product.</param>
+        /// <returns>Returns 0 if everything is okay.</returns>
         public async Task<int> DeleteAllReserve(string productName)
         {
             var reservations = _db.Reservations.Where(x => x.ProductName == productName);
